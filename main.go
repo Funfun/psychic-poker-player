4073ab9251cc757642263b866f661321fbe43b8c
Tsyren Ochirov <nsu1team@gmail.com>
2018-06-09 00:36:04 +0800

    Write result of the app to a file

--- main.go
+++ main.go
@@ -14,32 +14,51 @@ func check(e error) {
 }
 
 func main() {
-	var filename string
+	var inputFileName string
+	var outputFileName string
+
 	// read input file from argument
 	if len(os.Args) < 2 {
 		// when no file than use default
-		filename = "./sample_input"
+		inputFileName = "./sample_input"
+		outputFileName = "./output/result"
 	} else {
-		filename = os.Args[1]
+		inputFileName = os.Args[1]
+		outputFileName = os.Args[2]
 	}
 
-	f, err := os.Open(filename)
+	fi, err := os.Open(inputFileName)
 	check(err)
 
-	defer f.Close()
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
 
-	scanner := bufio.NewScanner(f)
+	fo, err := os.Create(outputFileName)
+	check(err)
+	w := bufio.NewWriter(fo)
+
+	scanner := bufio.NewScanner(fi)
 	for scanner.Scan() {
 		line := []rune(scanner.Text())
 		hand := string(line[:14])
 		deck := string(line[15:])
 
 		bestHand := FindBestHand(hand, deck)
-		fmt.Printf("Hand: %s Deck: %s Best hand: %s\n", hand, deck, bestHand)
+		output := fmt.Sprintf("Hand: %s Deck: %s Best hand: %s\n", hand, deck, bestHand)
+		if _, err := w.WriteString(output); err != nil {
+			log.Fatalf("Error while writing to a file. Error: %v", err)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	if err = w.Flush(); err != nil {
+		panic(err)
 	}
 
 }
