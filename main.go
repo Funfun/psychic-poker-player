4a43aef5d35687e1547bd1740394af86132c1c24
Tsyren Ochirov <nsu1team@gmail.com>
2018-06-08 19:31:51 +0800

    An ace ran low

--- main.go
+++ main.go
@@ -187,7 +187,7 @@ func HandCategoryName(rank int) string {
 
 func init() {
 	faceValueToIndex = map[string]int{"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
-		"T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+		"T": 10, "J": 11, "Q": 12, "K": 13, "A": 1}
 	suitToIndex = map[string]int{"C": 1, "D": 2, "H": 3, "S": 4}
 
 	HandCategories = make(map[string]int, 9)
