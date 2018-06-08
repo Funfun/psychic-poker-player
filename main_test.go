707cfae14967e6663e4ef9423650d700d0c2f1ec
Tsyren Ochirov <nsu1team@gmail.com>
2018-06-08 22:13:53 +0800

    Add top level test scenarios

--- main_test.go
+++ main_test.go
@@ -11,6 +11,38 @@ func TestFindBestHand(t *testing.T) {
 	hand := "TH JH QC QD QS"
 	deck := "QH KH AH 2S 6S"
 	assert.Equal(t, FindBestHand(hand, deck), "straight-flush")
+
+	hand = "2H 2S 3H 3S 3C"
+	deck = "2D 3D 6C 9C TH"
+	assert.Equal(t, FindBestHand(hand, deck), "four-of-a-kind")
+
+	hand = "2H 2S 3H 3S 3C"
+	deck = "2D 9C 3D 6C TH"
+	assert.Equal(t, FindBestHand(hand, deck), "full-house")
+
+	hand = "2H AD 5H AC 7H"
+	deck = "AH 6H 9H 4H 3C"
+	assert.Equal(t, FindBestHand(hand, deck), "flush")
+
+	hand = "AC 2D 9C 3S KD"
+	deck = "5S 4D KS AS 4C"
+	assert.Equal(t, FindBestHand(hand, deck), "straight")
+
+	hand = "KS AH 2H 3C 4H"
+	deck = "KC 2C TC 2D AS"
+	assert.Equal(t, FindBestHand(hand, deck), "three-of-a-kind")
+
+	hand = "AH 2C 9S AD 3CC"
+	deck = "QH KS JS JD KD"
+	assert.Equal(t, FindBestHand(hand, deck), "two-pairs")
+
+	hand = "6C 9C 8C 2D 7C"
+	deck = "2H TC 4C 9S AH"
+	assert.Equal(t, FindBestHand(hand, deck), "one-pair")
+
+	hand = "3D 5S 2H QD TD"
+	deck = "6S KH 9H AD QH"
+	assert.Equal(t, FindBestHand(hand, deck), "highest-card")
 }
 
 func TestFindHandCategory(t *testing.T) {
